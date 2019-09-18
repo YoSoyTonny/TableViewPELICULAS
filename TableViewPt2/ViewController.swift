2756efad0c24b98cc05f359f31ec36b7f02ebec3
Antonio <itonygame@gmail.com>
2019-09-18 13:54:19 -0700

    Initial Commit

---
+++ TableViewPt2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPt2
+//
+//  Created by equipo on 9/18/19.
+//  Copyright © 2019 equipo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
